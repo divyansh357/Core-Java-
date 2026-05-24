cc6c464ac12f55d853979f6321c74ec196da5bb1
Divyansh Kumar Gupta <divyanshkumargupta532@gmail.com>
2026-05-24 23:23:32 +0530

    super keyword

--- Hello.java
+++ Hello.java
@@ -699,3 +699,19 @@ class Student {
     
 }
 
+super Keyword
+class Animal {
+    String color;
+    Animal(){
+        System.out.println("Animal constructor is called");
+    }
+}
+
+class Horse extends Animal  {
+    Horse(){
+        super.color = "brown";
+        System.out.println("Horse constructor is called");
+    }
+}
+
+
