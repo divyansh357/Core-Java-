a572b4546751520d745ebb93e5731fae6ee1be38
Divyansh Kumar Gupta <divyanshkumargupta532@gmail.com>
2026-07-02 23:06:33 +0530

    Method Overloading

--- oops/Oops.java
+++ oops/Oops.java
@@ -1,17 +1,40 @@
 package oops;
 class Calculator{
+    int num; // instance variable
     public int add(int n1, int n2){
-        int result = n1 + n2;
-        return result;
+        return n1 + n2; // local variable
+    }
+    public int add(int n1,int n2, int n3){
+        return n1 + n2 + n3 ;
+    }
+    public double add(double n1 , double n2 , double n3){
+        return n1 + n2 + n3 ;
+    }
+}
+class Computer{
+    public void listenMusic(){
+        System.out.println("Listening to Music..");
+    }
+    public String getMeAPen(int cost){
+        if (cost>=10) {
+            return "Pen";
+        }
+        return "Nothing";
     }
 }
 public class Oops {
     public static void main(String[] args) {
         int num1 = 4;
         int num2 = 5;
+        int num3 = 7;
         Calculator calc = new Calculator();
-        int result = calc.add(num1, num2);
+        int result = calc.add(num1, num2,num3);
         System.out.println(result);
+        // Computer com = new Computer();
+        // com.listenMusic();
+        // String str = com.getMeAPen(2);   
+        // System.out.println(str);
+
     }
 
 }
