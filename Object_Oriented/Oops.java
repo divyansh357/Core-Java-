b03d959530a459af1f7ecadb681a3aae5993f15f
Divyansh Kumar Gupta <divyanshkumargupta532@gmail.com>
2026-08-28 11:20:01 +0530

    Object class- extended by every class in our codebase+

--- Object_Oriented/Oops.java
+++ Object_Oriented/Oops.java
@@ -163,6 +163,44 @@ class V extends Z{
 // }
 // }
 
+// Object Class
+class Laptop extends Object {// every class extends Object class implicitly
+    String model;
+    int price;
+    int serial;
+    @Override
+    public String toString() {
+        return "Laptop [model=" + model + ", price=" + price + "]";
+    }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((model == null) ? 0 : model.hashCode());
+        result = prime * result + price;
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Laptop other = (Laptop) obj;
+        if (model == null) {
+            if (other.model != null)
+                return false;
+        } else if (!model.equals(other.model))
+            return false;
+        if (price != other.price)
+            return false;
+        return true;
+    }
+    
+}
+
 
 public class Oops {
     public static void main(String[] args) throws ClassNotFoundException {
@@ -281,6 +319,21 @@ public class Oops {
         // Final class and methods
         // AdvCalc obj = new AdvCalc();
         // obj.add(1,2);
+
+        // Object Class
+        Laptop obj1 = new Laptop();
+        obj1.model ="Lenovo Yoga";
+        obj1.price = 1000;
+
+        Laptop obj2 = new Laptop();
+        obj2.model ="Lenovo Yoga";
+        obj2.price = 1000;
+        System.out.println(obj1); //Whenever we print the object, obj.toString() is called implicitly
+        // equals()
+        System.out.println(obj1.equals(obj2)); // overriden equals is being called 
+
+
+
 
 
 
